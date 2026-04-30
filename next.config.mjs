7aeb53f39dfdd7919374a7fd574b3e1d043c76f9
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-30 11:08:14 +0200

    x

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    useDeploymentId: true,
+    // Optionally, use with Server Actions
+    useDeploymentIdServerActions: true,
+  },
+};
