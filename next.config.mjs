4f9ab9879c79c7427222f2793831f8d4651e11f2
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-30 11:39:15 +0200

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    useDeploymentId: true,
-    // Optionally, use with Server Actions
-    useDeploymentIdServerActions: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
-};
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
